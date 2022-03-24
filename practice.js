dda19666a4d22afaa31eb56f6fd2a30e85507314
RikiRk <subhadiperchabi@gmail.com>
2022-03-24 23:00:42 +0530

    first

---
+++ practice.js
@@ -0,0 +1,97 @@
+// console.time(" ");
+// console.log("hello");
+// console.log(true);
+// console.log(false);
+// console.table({ subha: "this", marks: 34 });
+// console.table({ Riki: "this", marks: 3.4 });
+// console.warn("this is warning");
+// console.log(["a", "b", "c", "d"]);
+// console.error("this is error");
+// console.timeEnd(" ");
+/////////////////*
+
+// var nam = "Riki";
+// const name = "subha";
+// console.log(name, nam);
+
+// {
+//   let nam = "hello";
+//   console.log(nam);
+// }
+// console.log(nam);
+
+// const mark = [34,56,67,13,35,47];
+
+// const fruits = ["mango", "apple","banana"];
+
+// const mixed = [1,3,4,"Riki"];
+
+// console.log(mark);
+// console.log(mixed);
+// console.log(fruits);
+
+// console.log(mixed.length);
+// console.log(mixed[0]);
+
+// mixed[0]= "subha";
+
+// console.log(mixed)
+// console.log(mixed.indexOf("subha"));
+
+// let myobj = {
+//   name:"subhadip",
+//   age:22,
+//   job:false,
+//   skill:["react","angular"]
+
+// }
+
+// console.log(myobj);
+// const calculator = 4;
+// const firstNumber = 22;
+
+// const secondNumber = 23;
+
+// switch (calculator) {
+//   case 1:
+//     console.log(firstNumber + secondNumber);
+//     break;
+//   case 2:
+//     console.log(firstNumber - secondNumber);
+//     break;
+//   case 3:
+//     console.log(firstNumber * secondNumber);
+//     break;
+//   case 4:
+//     console.log(firstNumber / secondNumber);
+//     break;
+//   default:
+//     console.log("invalid number");
+//     break;
+// }
+
+// let arr = [1,2,3,4,5,6];
+
+// for (let i = 0; i < arr.length; i++) {
+//   const elememt = arr[i];
+//   console.log(elememt);
+// }
+
+// arr.forEach(function(element){
+//   console.log(element);
+// });
+
+// let obj = {
+//   name: "subha",
+//   age: 22,
+//   type: "hopeless engineer",
+// };
+
+// for (let key in obj) {
+//   console.log(`the ${key} of object is ${obj[key]}`);
+// }
+
+let a = document;
+let b = documemt.all;
+console.log(a);
+console.log(b);
